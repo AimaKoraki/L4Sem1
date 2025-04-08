c128aafdf3ba4cbd8fdb54f1d730731fc195d8d0
AimaKoraki <138265155+AimaKoraki@users.noreply.github.com>
2025-04-08 15:53:03 +0530

    added a thank you alert after paying

--- scripts/pay.js
+++ scripts/pay.js
@@ -1,8 +1,35 @@
 // Select the Pay button
 const payButton = document.getElementById('payBtn');
+const closePop = document.getElementById('closePopup');
 
 // Add Event Listener
-payButton.addEventListener('click', function(event) {
-    event.preventDefault(); // prevent form from submitting immediately
-    alert("Thank you for your payment!");
-});
+payButton.addEventListener("click", submitOrder);
+closePop.addEventListener("click", closePopup);
+
+function submitOrder(event) {
+    event.preventDefault(); // Prevent form from submitting
+
+    let Fname = document.getElementById("fName").value.trim();
+    let Lname = document.getElementById("lName").value.trim();
+    let address = document.getElementById("address").value.trim();
+    let cardNumber = document.getElementById("cardNumber").value.trim();
+
+    if (Fname === "" || address === "" || cardNumber === "" || Lname === "") {
+        alert("Please fill in all fields.");
+    } else {
+        // Display Thank You Popup
+        document.getElementById('thankYouMsg').innerText = `Thank you for your purchase, ${name}! Your order will be delivered soon.`;
+        document.getElementById('popup').style.display = "flex";
+
+        localStorage.removeItem("cart");
+
+        // Clear fields
+        document.getElementById("fName").value = "";
+        document.getElementById("address").value = "";
+        document.getElementById("cardNumber").value = "";
+    }
+}
+
+function closePopup() {
+    document.getElementById('popup').style.display = "none";
+}
