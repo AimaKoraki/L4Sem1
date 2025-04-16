6c4e29180a58765ba5ff8f99e67509b7d73cef9d
AimaKoraki <138265155+AimaKoraki@users.noreply.github.com>
2025-04-16 21:13:00 +0530

    added the webmanifest file again

--- scripts/hambugerger.js
+++ scripts/hambugerger.js
@@ -1,12 +1,12 @@
 
-// Hamburger menu script
-// This script is responsible for handling the hamburger menu functionality. It toggles the visibility of the menu when the hamburger button is clicked.
+// ------------------------Hamburger menu script ------------------------
+// ------------------------ This script is responsible for handling the hamburger menu functionality. It toggles the visibility of the menu when the hamburger button is clicked. ------------------------
 const btnHambuger = document.getElementById("hamButton"); 
 btnHambuger.addEventListener("click", hambuger);
 
 
-// Function to toggle the hamburger menu
-// This function checks the current class of the menu and toggles it between "header__ul" and "header__ul responsive". It also handles dropdown menus.
+//------------------------ Function to toggle the hamburger menu ------------------------
+// ------------------------This function checks the current class of the menu and toggles it between "header__ul" and "header__ul responsive". It also handles dropdown menus. ------------------------
 function hambuger() { 
   var x = document.getElementById("header__J"); 
   if (x.className === "header__ul") 
